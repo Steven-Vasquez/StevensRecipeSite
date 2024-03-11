3bd0b5a8576e97a16fc9781d8b8bbea8c2ac0093
steven vasquez <ministeven10@gmail.com>
2024-03-11 09:40:52 -0700

    side panel created

--- src/pages/Browsing.js
+++ src/pages/Browsing.js
@@ -1,9 +1,35 @@
-export function Browsing() {
+// Browsing.js
+import React, { useState } from "react";
+import "../stylesheets/Browsing.css";
+
+const Browsing = () => {
+  const [isSidePanelOpen, setIsSidePanelOpen] = useState(true);
+
+  const toggleSidePanel = () => {
+    setIsSidePanelOpen(!isSidePanelOpen);
+  };
+
   return (
-    <div>
-      <h1>Recipe browing page with side panel of filters</h1>
+    <div className={`page-container ${isSidePanelOpen ? "with-side-panel" : "without-side-panel"}`}>
+      {/* Side Panel for Filters */}
+      <div className="side-panel">
+        <h2>Filters</h2>
+        {/* Add filter options here */}
+      </div>
+
+      {/* Toggle Button */}
+      <div className="toggle-container">
+        <button className={`toggle-button `} onClick={toggleSidePanel}>
+          {isSidePanelOpen ? ">" : "<"}
+        </button>
+      </div>
+
+      {/* Main Content (Recipe List) */}
+      <div className="main-content">
+        <h1>Recipe browsing page with side panel of filters</h1>
+      </div>
     </div>
   );
-}
+};
 
 export default Browsing;
