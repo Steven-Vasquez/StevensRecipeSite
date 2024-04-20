4087494be23c920fb9962c1a7b4a8b3e130ac2bd
steven vasquez <ministeven10@gmail.com>
2024-04-19 20:49:21 -0700

    Updated css for recipe outline page. Still not too sure about the color palate. Maybe needs more contrast?

--- src/pages/RecipeOutline.js
+++ src/pages/RecipeOutline.js
@@ -6,26 +6,31 @@ export function RecipeOutline() {
     <div className="page-container">
 
       {/* Recipe Card Opening */}
-      <div className="recipe-card-opening">
-        <div className="recipe-name-container">
-          <h2 className="recipe-name">Fried Rice</h2>
-          <p className="recipe-credit">Recipe by: Me</p>
-          <div className="tags-container">
-            <span className="tag">Quick</span>
-            <span className="tag">Easy</span>
-            <span className="tag">One pot</span>
-            <span className="tag">Meal Prep</span>
+      <div className="outer-opening-container">
+        <div className="opening-card-container">
+
+          <div className="recipe-name-container">
+            <h2 className="recipe-name">Fried Rice</h2>
+            <p className="recipe-credit">Recipe by: Me</p>
+            <div className="tags-container">
+              <span className="tag">Quick</span>
+              <span className="tag">Easy</span>
+              <span className="tag">One pot</span>
+              <span className="tag">Meal Prep</span>
+            </div>
+          </div>
+
+
+
+          <div className="recipe-image-container">
+            <img src="/images/recipe_images/fried_rice/fried_rice.jpg" alt="Fried Rice" />
           </div>
         </div>
-
-        <div className="recipe-image-container">
-          <img src="/images/recipe_images/fried_rice/fried_rice.jpg" alt="Fried Rice" />
-        </div>
-
       </div>
 
       {/* Recipe Information */}
-      <div className="basic-info-container">
+      <div className="outer-info-container">
+        <div className="basic-info-container">
           <div className="recipe-info">
             <img src="/images/filter_icons/dish_type.png" alt="Dish Type" className="filter-icon" />
             <p>Dish Type</p>
@@ -66,8 +71,8 @@ export function RecipeOutline() {
               <li>None</li>
             </ul>
           </div>
-
         </div>
+      </div>
     </div>
   );
 }
