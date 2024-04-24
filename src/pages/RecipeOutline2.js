7099bf5ca5b1daf56fa8ded6f1ed045459ef184c
steven vasquez <ministeven10@gmail.com>
2024-04-23 18:23:15 -0700

    Grid transition successfully made

--- src/pages/RecipeOutline2.js
+++ src/pages/RecipeOutline2.js
@@ -9,23 +9,38 @@ export function RecipeOutline() {
             <div className="outer-opening-container">
                 <div className="opening-card-container">
 
-                    <div className="opening-card-left">
-                        <p className="recipe-name">Fried Rice</p>
-                        <p className="recipe-credit">Recipe by: Me</p>
-                        <div className="tags-container">
-                            <span className="tag">Quick</span>
-                            <span className="tag">Easy</span>
-                            <span className="tag">One pot</span>
-                            <span className="tag">Meal Prep</span>
+                    {/* Recipe Name and Tags */}
+                    <div className="outer-recipe-name-container">
+                        <div className="recipe-name-container">
+                            <p className="recipe-name">Fried Rice</p>
+                            <p className="recipe-credit">Recipe by: Me</p>
+                            <div className="tags-container">
+                                <span className="tag">Quick</span>
+                                <span className="tag">Easy</span>
+                                <span className="tag">One pot</span>
+                                <span className="tag">Meal Prep</span>
+                            </div>
+                            <p className="description-short">This is a simple fried rice recipe using easily-accessible supermarket ingredients</p>
                         </div>
-                        <p className="description-short">This is a simple fried rice recipe using easily-accessible supermarket ingredients</p>
                     </div>
 
-                    <div className="opening-card-right">
+                    {/* Recipe Image */}
+                    <div className="outer-recipe-image-container">
                         <div className="recipe-image-container">
                             <img src="/images/recipe_images/fried_rice/fried_rice.jpg" alt="Fried Rice" />
                         </div>
+                    </div>
 
+
+                    {/* Recipe Nutrition Information */}
+                    <div className="outer-nutrition-info-container">
+                        <div className="nutrition-info-container">
+                            nutrition info
+                        </div>
+                    </div>
+
+                    {/* Recipe Basic Info */}
+                    <div className="outer-basic-info-container">
                         <div className="basic-info-container">
                             <div className="recipe-info">
                                 <img src="/images/filter_icons/dish_type.png" alt="Dish Type" className="filter-icon" />
@@ -72,7 +87,6 @@ export function RecipeOutline() {
 
                 </div>
             </div>
-
         </div>
     );
 }
