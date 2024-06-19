366948c453bff4c91d7e2239e0c82ae1081247a9
steven vasquez <ministeven10@gmail.com>
2024-06-19 10:08:24 -0700

    Progress on the new opening card based on Betty Crocker's design

--- src/pages/RecipeOutline3.js
+++ src/pages/RecipeOutline3.js
@@ -5,7 +5,29 @@ export function RecipeOutline() {
         <div className="recipe-outline-container">
             <div className="opening-card-container">
                 <div className="left">
-                    left container
+                    <p className="recipe-name">Korean Fried Yellow Croaker</p>
+                    
+                    {/* Tags */}
+                    <div className="tags-container">
+                        <span className="tag">Quick</span>
+                        <span className="tag">Easy</span>
+                        <span className="tag">One pan</span>
+                        <span className="tag">Korean</span>
+                    </div>
+
+                    <div className="recipe-attributes">
+                        <ul>
+                            <li className="attribute">
+                                test
+                            </li>
+                            <li className="attribute">
+                                test
+                            </li>
+                            <li className="attribute">
+                                test
+                            </li>
+                        </ul>
+                    </div>
                 </div>
 
                 <div className="right">
